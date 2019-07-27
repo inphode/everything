85fed68c7840047ccf29b2db31fcba182ca74703
Phill Quantrill <phill.quantrill@netsells.co.uk>
2019-07-27 13:13:20 +0100

    Adds installers folder with xcape for caps lock rebinding

---
+++ installers/xcape/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install git gcc make pkg-config libx11-dev libxtst-dev libxi-dev -y
+[[ -d /tmp/installers/xcape ]] && rm -rf /tmp/installers/xcape
+mkdir -p /tmp/installers/xcape
+git clone https://github.com/alols/xcape.git /tmp/installers/xcape
+cd /tmp/installers/xcape
+make
+sudo make install
+
---
+++ installers/xcape/uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+[[ -f /usr/local/man/man1/xcape.1 ]] && rm -f /usr/local/man/man1/xcape.1
+[[ -f /usr/bin/xcape ]] && rm -f /usr/bin/xcape
+

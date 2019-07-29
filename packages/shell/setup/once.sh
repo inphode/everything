97e98c647b2d228be8f948a15292e12b161bd78d
Phill <phillquantrill@gmail.com>
2019-07-29 08:14:25 +0100

    Makes monospace font get set in Gnome

--- packages/shell/setup/once.sh
+++ packages/shell/setup/once.sh
@@ -2,6 +2,9 @@
 
 sudo apt install -y rxvt-unicode fonts-inconsolata terminator
 
+# Set gnome monospace font to Inconsolata
+gsettings set org.gnome.desktop.interface monospace-font-name 'Inconsolata 12'
+
 # Install gruvbox theme for Terminator terminal
 mkdir -p ~/.config/terminator/ && wget -O ~/.config/terminator/config https://raw.githubusercontent.com/egel/terminator-gruvbox/master/config
 

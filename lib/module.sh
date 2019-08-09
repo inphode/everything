95d07462bd5fba1caa2082a3495ac7031a2ed2ef
Phill <phillquantrill@gmail.com>
2019-08-09 07:25:58 +0100

    Adds function stubs for module lib

--- lib/module.sh
+++ lib/module.sh
@@ -3,7 +3,28 @@
 # Export all variables and functions
 set -a
 
+if [[ -z $MODULE_LIST_FILE ]]; then
+    MODULE_LIST_FILE="$EVERYTHING_PATH/modules.list"
+fi
 
+if [[ -z $MODULE_LIST ]]; then
+    MODULE_LIST=$(cat "$MODULE_LIST_FILE" | xargs)
+fi
+
+function module_verify {
+}
+
+function module_enable {
+}
+
+function module_verify_enable {
+}
+
+function module_disable {
+}
+
+function module_verify_disable {
+}
 
 set +a
 

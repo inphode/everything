e68c338f06c7b46590f0781f3900895ea859daab
Phill Quantrill <phill.quantrill@netsells.co.uk>
2019-08-09 09:56:47 +0100

    Adds sync function stubs and untested sync_all

--- lib/module.sh
+++ lib/module.sh
@@ -26,5 +26,21 @@ function module_disable {
 function module_verify_disable {
 }
 
+function module_sync {
+}
+
+function module_sync_all { $options=$@
+    force=$([[ $options =~ "force" ]] && echo 1)
+
+    for module_line in $MODULE_LIST
+    do
+        echo -e "\033[36m  Synchronising module: $module_line\033[0m"
+        module_sync $module_line
+        if [[ $? != 0 && $force != 1 ]]; then
+            echo -e "\033[36m  Sync stopped by failed module: $module_line\033[0m"
+        fi
+    done
+}
+
 set +a
 

91b6403050c3fe6406169c25d8773c8a392f9022
Phill <phillquantrill@gmail.com>
2020-02-28 17:33:59 +0000

    Adds module new/modify command stubs

--- lib/command.sh
+++ lib/command.sh
@@ -51,6 +51,20 @@ function command_module_sync { modules=$@
     module_sync $modules
 }
 
+# Create a new module in specified packages with specified module name.
+# package: package name of the package to create the module in
+# module: name of the module to be created
+function command_module_new { module=$2; package=$1
+}
+
+# Modify an existing package. Opens enable, disable and verify scripts
+# in text editor. If you don't specify a package name, it will look for
+# the module in the insalled modules directory.
+# module: name of the module to edit
+# package: (optional) package name where the module is to be found
+function command_module_modify { module=$2; package=$1
+}
+
 # Restore backed up package conflicts.
 # serials: optional list of backups to restore
 function command_restore { serials=$@

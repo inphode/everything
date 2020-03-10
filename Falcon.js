9918f46fe878e828356263a9e2ec434869df0430
Phill <phillquantrill@gmail.com>
2020-03-10 23:01:26 +0000

    Adds Falcon blink.sh colour scheme for the purpose of importing

---
+++ Falcon.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000004", "#ff3600", "#718e3f", "#ffc552", "#635196", "#ff761a", "#34bfa4", "#b4b4b9", "#020221", "#ff8e78", "#b1bf75", "#ffd392", "#99a4bc", "#ffb07b", "#8bccbf", "#f8f8ff"]);
+t.prefs_.set('foreground-color', "#b4b4b9");
+t.prefs_.set('background-color', "#020221");
+t.prefs_.set('cursor-color', '#ffe8c0');

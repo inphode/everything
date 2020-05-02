f06913882802d916055faef5d36d38cee67998ca
Phill <phillquantrill@gmail.com>
2020-05-02 20:33:31 +0100

    Adds backup and restore for database and git repositories

--- packages/serversidesquid/bin/serversidesquid-backup.sh
+++ packages/serversidesquid/bin/serversidesquid-backup.sh
@@ -18,7 +18,27 @@ set -a
 . "$SSS_ENV"
 set +a
 
-# TODO: Implement full database SQL backup here
+BACKUP_DIR="$SSS_EVERYTHING_SYNC/backups/$(date +%Y-%m-%d--%H-%M-%S)"
+mkdir -p "$BACKUP_DIR/sql"
+mkdir -p "$BACKUP_DIR/git"
 
-# TODO: Implement git non-version controlled file backup here
+# Database backup
+databases=$(mysql -u $SSS_DB_USER -p$SSS_DB_PASS -e "SHOW DATABASES;" 2>/dev/null | tr -d "| " | grep -v Database)
+
+for db in $databases; do
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "sys" ]]; then
+        echo "Dumping database: $db"
+        mysqldump -u $SSS_DB_USER -p$SSS_DB_PASS --databases $db 2>/dev/null | gzip > "$BACKUP_DIR/sql/$db.sql.gz"
+    fi
+done
+
+# Git repository backup
+repositories=$(ls "$SSS_GIT_DIR" | xargs)
+
+for repo in $repositories; do
+    if [[ -d "$SSS_GIT_DIR/$repo/.git" ]]; then
+        echo "Backing up repo: $repo"
+        ( cd $SSS_GIT_DIR && tar czf "$BACKUP_DIR/git/$repo.tar.gz" "$repo" )
+    fi
+done
 
--- packages/serversidesquid/bin/serversidesquid-restore.sh
+++ packages/serversidesquid/bin/serversidesquid-restore.sh
@@ -18,7 +18,29 @@ set -a
 . "$SSS_ENV"
 set +a
 
-# TODO: Implement full database SQL restore here
+LATEST_BACKUP=$(ls "$SSS_EVERYTHING_SYNC/backups" | tail -n 1)
+BACKUP_DIR="$SSS_EVERYTHING_SYNC/backups/$LATEST_BACKUP"
 
-# TODO: Implement git non-version controlled file restore here
+# Database restore
+databases=$(ls "$BACKUP_DIR/sql" | xargs)
+
+for db in $databases; do
+    db=${db%%.*}
+    echo "Restoring database: $db"
+    mysql -u $SSS_DB_USER -p$SSS_DB_PASS -e "CREATE DATABASE IF NOT EXISTS $db CHARACTER SET utf8 COLLATE utf8_unicode_ci;" 2>/dev/null
+    zcat "$BACKUP_DIR/sql/$db.sql.gz" | mysql -u $SSS_DB_USER -p$SSS_DB_PASS $db 2>/dev/null
+done
+
+# Git repository restore
+repositories=$(ls "$BACKUP_DIR/git" | xargs)
+
+for repo in $repositories; do
+    repo=${repo%%.*}
+    if [[ -d "$SSS_GIT_DIR/$repo" ]]; then
+        echo "Removing existing target repo directory: $repo"
+        rm -rf "$SSS_GIT_DIR/$repo"
+    fi
+    echo "Restoring repo: $repo"
+    tar xzf "$BACKUP_DIR/git/$repo.tar.gz" -C "$SSS_GIT_DIR/"
+done
 
